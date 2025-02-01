1a70d42edecb97251fd48341cc977f8c60f58921
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-02-01 20:54:40 +0100

    Update and rename rain_v1_kodland.py to rain_v1.1_kodland.py

--- rain_v1.1_kodland.py
+++ rain_v1.1_kodland.py
@@ -8,13 +8,18 @@ window.tracer(0)
 t=turtle.Turtle()
 window.bgcolor("black")
 drops=[]
-for i in range(100):
-    drops.append([random.randint(0-window.window_width()/2,window.window_width()/2),window.window_height()/2])
-
+# for i in range(100):
+#     drops.append([random.randint(0-window.window_width()/2,window.window_width()/2),window.window_height()/2])
+#
 
 
 while True:
     t.clear()
+
+    if len(drops)<100:
+        if random.choice([True,False]):
+            drops.append([random.randint(0-window.window_width()/2,window.window_width()/2),window.window_height()/2])
+            time.sleep(random.randint(1,5)/100)
     for i in drops[:]:
         if i[1] <= 0-window.window_height()/2:
             drops.pop(drops.index(i))
